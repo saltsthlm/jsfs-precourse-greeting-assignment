032c6db8b89b607efdb2e4a00a392a6cbbb3eb27
Marcus Rådell <marcus@radell.net>
2024-09-04 14:55:00 +0200

    setup failing test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,10 @@
 import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
-test("should work", () => {
-  deepEqual(1, 1);
+function greet() {}
+
+test("should greet person", () => {
+  const result = greet("Alek");
+
+  deepEqual(result, { message: "Welcome, Alek!" });
 });
